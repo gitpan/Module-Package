31faac5bba420a870aa76664ade74432a5c6b483
Ingy döt Net <ingy@cpan.org>
2011-05-27 18:31:28 +0000

    Import of INGY/Module-Package-0.12 from CPAN.
    gitpan-cpan-distribution: Module-Package
    gitpan-cpan-version:      0.12
    gitpan-cpan-path:         INGY/Module-Package-0.12.tar.gz
    gitpan-cpan-author:       INGY
    gitpan-cpan-maturity:     released

--- lib/inc/Module/Package.pm
+++ lib/inc/Module/Package.pm
@@ -1,15 +1,23 @@
 ##
-# name:      inc::Package
-# abstract:  inc::Module::Install on Kool-aid. Oh Yeah!
+# name:      inc::Module::Package
+# abstract:  Module::Package Bootstrapper
 # author:    Ingy döt Net <ingy@ingy.net>
 # license:   perl
 # copyright: 2011
 
 package inc::Module::Package;
-$VERSION = '0.11';
-{
+
+BEGIN {
+    $VERSION = '0.12';
     package main;
-    use Module::Package;
+    use Module::Package 0.12 ();
+    die "Module::Package Bootstrapping Error"
+        unless $Module::Package::VERSION eq $inc::Module::Package::VERSION;
+}
+
+sub import {
+    my $class = shift;
+    Module::Package->import(@_);
 }
 
 1;
@@ -18,12 +26,12 @@ $VERSION = '0.11';
 
 In you C<Makefile.PL>:
 
-    use inc::Package;
+    use inc::Module::Package <options>;
 
 =head1 DESCRIPTION
 
-This is the inc::Module::Package bootstrapping module.
-It works something like inc::Module::Install.
-This code should only be loaded in an author environment.
+This is the L<Module::Package> bootstrapping module.  It works something like
+L<inc::Module::Install>.  This bootstrap module should only be loaded in an
+author environment.
 
 See L<Module::Package> for more information.
